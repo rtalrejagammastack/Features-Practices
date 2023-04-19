9bec628b83c591d8e40f7e401fa539b8ca211e29
rtalrejagammastack <rtalreja@grepruby.io>
2023-04-19 23:58:19 +0530

    COntroller Model Css done

--- quote-editor/db/seeds.rb
+++ quote-editor/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "\n== Seeding the database with fixtures =="
+system("rails db:fixtures:load")
+
+# run command
+# rails db:migrate
