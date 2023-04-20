a71d9a6113a362275eae4381594db4faee0ae0e9
rtalrejagammastack <rtalreja@grepruby.io>
2023-04-20 09:11:57 +0530

    Added Turbo

--- quote-editor/app/javascript/packs/application.js
+++ quote-editor/app/javascript/packs/application.js
@@ -7,7 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+// import { Turbo } from "@hotwired/turbo-rails"
+// Turbo.session.drive = false
+import "@hotwired/turbo-rails"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "../controllers"
